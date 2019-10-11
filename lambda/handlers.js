9ba21f2f086aa6c44ccc84710ae7cef84f9ba288
Tim Holmes <tim.holmes.external@ynap.com>
2019-10-11 10:21:07 +0100

    Fix AWS Xray support - plus bugfix

--- lambda/handlers.js
+++ lambda/handlers.js
@@ -16,19 +16,18 @@ const
     middy = require('middy'),
     stringify = require('json-stringify-safe'),
     truthy = require('truthy'),
-    AWSXRay = require('aws-xray-sdk'),
     unirest = require('unirest'),
     pkce = require('./pkce'),
     httpHeaderDefaults = require('./middleware/httpHeaderDefaults'),
     stringifyJsonResponse = require('./middleware/stringifyJsonResponse'),
     logTrace = require('./middleware/logTrace'),
+    awsXRay = require('./middleware/awsXRay'),
     jwt = require('jsonwebtoken'),
     jwks = require('jwks-rsa'),
     { httpHeaderNormalizer, jsonBodyParser, httpErrorHandler, urlEncodeBodyParser, httpPartialResponse } = require('middy/middlewares');
 
-AWSXRay.captureHTTPsGlobal(require('http'));
 pkce.setTableName(process.env.VERIFIER_TABLE_NAME);
-let jwksClients = {};
+let jwksClients = {}; // TODO refactor to use a proper managed cache
 
 var log = function () { if (truthy(process.env.DEBUG)) { console.log(...arguments) } };
 
@@ -114,27 +113,33 @@ let private = {
         try {
             status = 200;
             decoded = jwt.decode(token, { complete: true });
-            log('jwtdecode', decoded);
+            log('jwtdecode - unverified token', stringify(decoded));
             let verify = handler.event.queryStringParameters && truthy(handler.event.queryStringParameters.verify);
-            log('jwtdecode', verify, handler.event.queryStringParameters);
+            log('jwtdecode', stringify({verified: verify, params: handler.event.queryStringParameters}));
 
             if (verify) {
-                let jwksClient = findJwksClient(decoded.payload.iss);
+                log('jwtdecode - resolving jwksClient');
+                let jwksClient = private.findJwksClient(decoded.payload.iss);
+                log('jwtdecode - resolved jwksClient for', decoded.payload.iss);
                 let getKey = (header, callback) => {
                     jwksClient.getSigningKey(header.kid, (err, key) => {
                         if (key) {
                             log('jwtdecode key ', key);
                             var signingKey = key.publicKey || key.rsaPublicKey;
                             if (signingKey) {
+                                log('jwtdecode found public key', stringify(signingKey));
                                 callback(null, signingKey);
                             } else {
+                                log('jwtdecode - error - no public key for KID');
                                 callback(err, "No public key for KID");
                             }
                         } else {
+                            log('jwtdecode - error - no key passed to callback');
                             callback(err, "No public key for KID");
                         }
                     });
                 };
+                log('jwtdecode - verifying token');
                 const psVerify = (token) => {
                     return new Promise((resolve, reject) => {
                         jwt.verify(token, getKey, (err, data) => {
@@ -149,6 +154,7 @@ let private = {
             }
         } catch (e) {
             status = 500;
+            log('jwtdecode - error', e);
             decoded = e;
         }
         return {
@@ -182,6 +188,7 @@ const eventHandler = async (event, context) => {
 const defaultHeaders = { Authorization: `Basic ${Buffer.from(process.env.GIGYA_CLIENT_ID + ":" + process.env.GIGYA_CLIENT_SECRET).toString('base64')}` };
 
 let handler = middy(eventHandler)
+    .use(awsXRay())
     .use(httpHeaderNormalizer())
     .use(httpHeaderDefaults(defaultHeaders))
     .use(jsonBodyParser())
